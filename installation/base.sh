cb2cde7246da34df9c01f81bbdf0e6d065bb8245
Egide <egide.github@rnv.ch>
2014-10-17 21:08:32 +0200

    Ajout script d'installation
    Création du fichier... Et tergiversation...

---
+++ installation/base.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Intro {{{1
+#=============================================== vim: set fdm=marker: ====
+#
+#          FILE:  base.sh
+# 
+#         USAGE:  ./base.sh 
+# 
+#   DESCRIPTION:  Script d'installation pour archlinux
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Egide Dey egide.github@rnv.ch
+#       COMPANY:  Ponthaux, Suisse
+#       CREATED:  17. 10. 14 20:45:08 CEST
+#        SOURCE:  https://github.com/EgideMaison/PadmaPortableJournal
+#=========================================================================
+# /Intro }}}1
+
+
