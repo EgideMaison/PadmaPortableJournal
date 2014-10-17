a4c70402b93c00b4595f4527f5026c94bf86493f
Egide <egide.github@rnv.ch>
2014-10-17 21:49:35 +0200

    Partition de boot

--- installation/base.sh
+++ installation/base.sh
@@ -19,9 +19,15 @@
 #=========================================================================
 # /Intro }}}1
 
-# Partitionnement {{{1
+# Préparation des disques {{{1
+
+# Partitionnement {{{2
 # Avec cfdisk, création de deux partitions :
 # /dev/sda1   2048-514047        512000     250M 83 Linux
 # /dev/sda2 514048-488397167  487883120   232.7G 8e Linux LVM
 
 # la première pour le boot, la deuxième pour LVM
+
+# Partition de boot {{{2
+mkfs.ext2 /dev/sda1
+
