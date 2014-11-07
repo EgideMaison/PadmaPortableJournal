e0594f6db903385d2fbd4a79f84e6ede821951c9
Egide <egide.github@rnv.ch>
2014-11-07 08:53:56 +0100

    Commandes d'origines dans des fonctions
    Passé les commandes originales dans des fonctions pour en simplifier la
    désactivations.

--- installation/base.sh
+++ installation/base.sh
@@ -34,35 +34,43 @@ exit 0
 
 # Préparation des disques {{{1
 
-# Partitionnement {{{2
-# Avec cfdisk, création de deux partitions :
-# /dev/sda1   2048-514047        512000     250M 83 Linux
-# /dev/sda2 514048-488397167  487883120   232.7G 8e Linux LVM
+# Partitionnement du disque {{{2
+fpartitions(){
+	# Avec cfdisk, création de deux partitions :
+	# /dev/sda1   2048-514047        512000     250M 83 Linux
+	# /dev/sda2 514048-488397167  487883120   232.7G 8e Linux LVM
 
-# la première pour le boot, la deuxième pour LVM
+	# la première pour le boot, la deuxième pour LVM
+}
 
-# Partition de boot {{{2
-mkfs.ext2 /dev/sda1
+# Préparation des partition LVM {{{2
+flvm(){
+	# création du groupe physique
+	pvcreate /dev/sda2
 
-# LVM {{{2
-# création du groupe physique
-pvcreate /dev/sda2
+	# création du groupe logique
+	vgcreate ddsdd /dev/sda2
 
-# création du groupe logique
-vgcreate ddsdd /dev/sda2
+	# Création des volumes logiques
+	lvcreate -L 20G ddsdd -n racine
+	lvcreate -L 150G ddsdd -n docs
+}
 
-# Création des volumes logiques
-lvcreate -L 20G ddsdd -n racine
-lvcreate -L 150G ddsdd -n docs
-
-mkfs.ext4 -L racine /dev/ddsdd/racine
-mkfs.ext4 -L docs /dev/ddsdd/docs
+# Formatage des partitions {{{2
+fformatage(){
+	mkfs.ext2 /dev/sda1
+	mkfs.ext4 -L racine /dev/ddsdd/racine
+	mkfs.ext4 -L docs /dev/ddsdd/docs
+}
 
 # Montage de disques {{{2
-mount /dev/ddsdd/racine /mnt
+fmontagedisque(){
+	# Créations des dossiers racines pour les montages
+	mkdir -p /mnt/{boot,home}
+	mount /dev/sda1 /mnt/boot
+	mount /dev/ddsdd/docs /mnt/home
+	mount /dev/ddsdd/racine /mnt
+}
 
-# Créations des dossiers racines pour les montages
-mkdir -p /mnt/{boot,home}
-mount /dev/sda1 /mnt/boot
-mount /dev/ddsdd/docs /mnt/home
+# }}}1
 
